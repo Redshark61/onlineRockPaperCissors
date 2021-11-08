39bb3897c66ed2b47e2aef0259025cd1844824f4
Redshark61 <tim.vannier@gmail.com>
2021-11-08 18:31:31 +0100

    add a menu screen

--- client.py
+++ client.py
@@ -118,4 +118,27 @@ def main():
         redrawWindow(win, game, player)
 
 
-main()
+def menuScreeen():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+        win.fill((128, 128, 128))
+        font = pygame.font.SysFont("comicsans", 60)
+        text = font.render("Click to Play!", 1, (255, 0, 0))
+        win.blit(text, (100, 200))
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                run = False
+
+    main()
+
+
+while True:
+    menuScreeen()
